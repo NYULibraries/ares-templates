1fa66657938b7c5a7201a639a39ef6c123f2103c
BAlter <barnaby.alter@nyu.edu>
2019-01-25 11:29:51 -0500

    Add option to add files to testweb env first

--- ftp.sh
+++ ftp.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-# Usage: ./upload.sh myUser 'myPa55w0rd!' '*.html'
+# Usage: ./upload.sh myUser 'myPa55w0rd!' '*.html' [testweb]
 
 USERNAME=$1
 PASSWORD=$2
 PATTERN=$3
+ENV=$4
 HOST="sftp://ares.library.nyu.edu"
 
-lftp -u $USERNAME,$PASSWORD -e "cd RemoteAuth; mirror -R ./dist/custom ./custom; mput ./dist/$PATTERN; exit" $HOST
+if [[ $ENV == 'testweb' ]]; then
+  ENV_FOLDER='/TestWeb'
+else
+  ENV_FOLDER=''
+fi
+
+lftp -u $USERNAME,$PASSWORD -e "cd RemoteAuth$ENV_FOLDER; mirror -R ./dist/custom ./custom; mput ./dist/$PATTERN; exit" $HOST
