bcbf38fe54d5db07f34ec2ef600f24b553997efe
BAlter <barnaby.alter@nyu.edu>
2019-01-25 16:04:46 -0500

    Additional a11y edits

--- dist/custom/nyulibraries.js
+++ dist/custom/nyulibraries.js
@@ -1,5 +1,7 @@
 $(function() {
     $('input[type="text"], input[type="checkbox"]').not('[aria-label]').each( function() { $(this).attr('aria-label', $(this).attr('title')) } );
+    $('input[type="checkbox"]').not('[aria-label]').each( function() { $(this).attr('aria-label', $(this).attr('aria-description')) } );
+    $('input[aria-description]').each( function() { $(this).removeAttr('aria-description') });
     $('#ItemTable').find('[id^="ItemTags"]').filter(':empty').html($('<span />').addClass('sr-only').html('no tags'));
     $('td:empty').each( function() { $(this).html($('<span />').addClass('sr-only').html('no content')) });
     $('th:empty').each( function() { $(this).html($('<span />').addClass('sr-only').html($(this).attr('aria-label'))) });
