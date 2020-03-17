fc8b4230d37593c947eb1ecbdb0a5dd92812e94c
Eric Griffis <eric.griffis@nyu.edu>
2020-03-17 14:07:22 -0400

    Namespace credentials env vars

--- Dockerfile
+++ Dockerfile
@@ -7,8 +7,8 @@ RUN apk --no-cache add lftp openssh
 
 USER docker
 
-ENV ARES_HOST "ares.library.nyu.edu"
-RUN mkdir ~/.ssh && ssh-keyscan -H "$ARES_HOST">> ~/.ssh/known_hosts
+ENV ARES_HOSTNAME "ares.library.nyu.edu"
+RUN mkdir ~/.ssh && ssh-keyscan -H "$ARES_HOSTNAME" >> ~/.ssh/known_hosts
 
 WORKDIR /app
 COPY . .
--- ftp.sh
+++ ftp.sh
@@ -1,19 +1,17 @@
 #!/bin/sh -e
 
 # Usage: ./upload.sh '*.html' [prod]
-# Must set $USERNAME and $PASSWORD as environment variables
+# Must set $SFTP_USERNAME and $SFTP_PASSWORD as environment variables
 
-if [ -z "$USERNAME" ] || [ -z "$PASSWORD" ]
+if [ -z "$SFTP_USERNAME" ] || [ -z "$SFTP_PASSWORD" ]
 then
-  echo "Must specify \$USERNAME and \$PASSWORD for authentication"
+  echo "Must specify \$SFTP_USERNAME and \$SFTP_PASSWORD for authentication"
   exit 1
 fi
 
-#USERNAME=$1
-#PASSWORD=$2
 PATTERN=$1
 ENV=$2
-SFTP_HOST="sftp://$ARES_HOST"
+SFTP_HOST="sftp://$ARES_HOSTNAME"
 
 if [ -n "$ENV" ] && [[ $ENV == 'prod' ]]
 then
@@ -22,5 +20,5 @@ else
   ENV_FOLDER='/TestWeb'
 fi
 
-lftp -u $USERNAME,$PASSWORD -e "cd RemoteAuth$ENV_FOLDER; mirror -R ./dist/custom ./custom; mput ./dist/$PATTERN; exit" $SFTP_HOST
-lftp -u $USERNAME,$PASSWORD -e "cd AresAuth$ENV_FOLDER; mirror -R ./dist/custom ./custom; mput ./dist/$PATTERN; exit" $SFTP_HOST
+lftp -u $SFTP_USERNAME,$SFTP_PASSWORD -e "cd RemoteAuth$ENV_FOLDER; mirror -R ./dist/custom ./custom; mput ./dist/$PATTERN; exit" $SFTP_HOST
+lftp -u $SFTP_USERNAME,$SFTP_PASSWORD -e "cd AresAuth$ENV_FOLDER; mirror -R ./dist/custom ./custom; mput ./dist/$PATTERN; exit" $SFTP_HOST
