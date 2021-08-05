3b7dc904e54bcea3c3c0f9c08b4fb3e49f4c302d
Eric Griffis <eric.griffis@nyu.edu>
2021-08-05 15:20:55 -0400

    Circle - add test to check that compile script ran

---
+++ test_compile.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+dist_dir=hosting-ares-nyu
+
+if [[ -z "$1" ]]; then
+  echo "Usage: "
+  echo " $0 <env>"
+  exit
+fi
+
+if [[ " dev prod " != *" $1 "* ]]; then
+  echo "Invalid env, must be 'dev' or 'prod'"
+  exit 1
+fi
+
+./compile.sh $1
+git -C $dist_dir update-index --refresh
+if ! git -C $dist_dir diff-index --quiet HEAD --; then
+  echo "Failed test: compile $1 show uncommited changes to dist ($dist_dir)"
+  exit 1
+else
+  echo "Success! Compile $1 shows no changes"
+fi
